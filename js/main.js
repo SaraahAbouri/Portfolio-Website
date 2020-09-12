f27ecd8e8835239530cf1951d5d5f2f50025e9c2
SaraahAbouri <saraahabouri.sa@gmail.com>
2020-09-12 18:20:34 +0200

    added some more code to my html files

--- js/main.js
+++ js/main.js
@@ -20,4 +20,41 @@ $('.project-area .grid .test-popup-link').magnificPopup({
     type: 'image',
     gallery:{enabled:true}
   });
+
+//   owl-carousel
+
+$('.site-main .about-area .owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        544:{
+            items:2
+        }
+    }
+
+})
+
+// sticky navigation menu
+
+let nav_offset_top = $('.header_area').height() + 50;
+
+function navbarFixed(){
+    if($('.header_area').length){
+        $(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if(scroll >= nav_offset_top){
+              $('.header_area .main-menu').addClass('navbar_fixed');
+            }else{
+              $('.header_area .main-menu').removeClass('navbar_fixed');
+            }
+        })
+    }
+}
+navbarFixed()
+
 });
+
